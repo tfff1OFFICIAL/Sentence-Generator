0b3bcd734f6481727a771c4836aa1af6e2b3f3fb
Thomas Finlay <Thomas Finlay>
2016-07-05 17:20:22 +1200

    Added Functionality, still need to add actual sentences though...
    Signed-off-by: Thomas Finlay <Thomas Finlay>

--- popup.js
+++ popup.js
@@ -1,3 +1,23 @@
+var start = ["a", "b", "c", "d", "e"];
+var end = ["a", "b", "c", "d", "e"];
+
 function generateSentence() {
-	
+    var element = document.getElementById("generatedSentence");
+    element.innerHTML = start[randomNumber()].toString() + " " + end[randomNumber()].toString() + "<br>";
 }
+
+function randomNumber() {
+    var max = start.length - 1;
+    var min = 0;
+           
+    var Number = Math.floor(Math.random()*(max-min+1)+min);
+    console.log("Number is: " + Number.toString());
+    return Number;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById("generateBtn");
+    button.addEventListener("click", function() {
+        generateSentence();
+    });
+});
